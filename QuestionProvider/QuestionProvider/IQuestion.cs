ca3e43b958ce0ff9e27243a0582e139a0df15bf0
stefan <stefan.masznitz@gmx.at>
2020-02-09 10:16:09 +0100

    scaffolding questions

---
+++ QuestionProvider/QuestionProvider/IQuestion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuestionProvider
+{
+    public interface IQuestion
+    {
+        string Text { get; }
+        int Difficulty { get; }
+        IAnswerCollection Solution { get; }
+        double Eveluate(IAnswerCollection answers);
+    }
+}
---
+++ QuestionProvider/QuestionProvider/Questions/FreeTextQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuestionProvider.Questions
+{
+    public class FreeTextQuestion : IQuestion
+    {
+        public string Text => throw new NotImplementedException();
+
+        public int Difficulty => throw new NotImplementedException();
+
+        public IAnswerCollection Solution => throw new NotImplementedException();
+
+        public double Eveluate(IAnswerCollection answers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
