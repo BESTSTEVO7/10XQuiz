5a72e25f5fc7e93f4a707e084a995dc258bf0abf
stefan <stefan.masznitz@gmx.at>
2020-02-16 18:48:03 +0100

    - added analyzers to props for all project in the solution

--- src/Quiz.Logic/Program.cs
+++ src/Quiz.Logic/Program.cs
@@ -1,12 +1,14 @@
-﻿using System;
-
-namespace Quiz.Logic
+﻿namespace Quiz.Logic
 {
-    class Program
+    using System;
+
+    public sealed class Program
     {
         static void Main(string[] args)
         {
+#pragma warning disable CA1303 // Literale nicht als lokalisierte Parameter übergeben
             Console.WriteLine("Hello World!");
+#pragma warning restore CA1303 // Literale nicht als lokalisierte Parameter übergeben
         }
     }
 }
