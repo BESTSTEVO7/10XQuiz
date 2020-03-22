e5b57af2a18d8100e7ddc92598349d7c361f69fd
stefan <stefan.masznitz@gmx.at>
2020-03-22 12:00:53 +0100

    Added more test cases to ChooseQuestionFactoryTest

--- test/Quiz.Logic.Test/Factories/ChooseQuestionFactoryTest.cs
+++ test/Quiz.Logic.Test/Factories/ChooseQuestionFactoryTest.cs
@@ -10,13 +10,14 @@
         public void Create_OneQuestionTrue_ReturnsQuestion()
         {
             // Arrange
+            int amount = 1;
             TrueAnswers trueAnswers = TrueAnswers.FirstAnswer;
 
             // Act
-            IChooseQuestion actual = ChooseQuestionFactory.Create(0, 1, trueAnswers);
+            IChooseQuestion actual = ChooseQuestionFactory.Create(0, amount, trueAnswers);
 
             // Assert
-            actual.Answers.Count.Should().Be(1);
+            actual.Answers.Count.Should().Be(amount);
             actual.Answers[0].IsCorrect.Should().BeTrue();
         }
 
@@ -24,13 +25,14 @@
         public void Create_OneQuestionFalse_ReturnsQuestion()
         {
             // Arrange
+            int amount = 1;
             TrueAnswers trueAnswers = TrueAnswers.None;
 
             // Act
-            IChooseQuestion actual = ChooseQuestionFactory.Create(0, 1, trueAnswers);
+            IChooseQuestion actual = ChooseQuestionFactory.Create(0, amount, trueAnswers);
 
             // Assert
-            actual.Answers.Count.Should().Be(1);
+            actual.Answers.Count.Should().Be(amount);
             actual.Answers[0].IsCorrect.Should().BeFalse();
         }
 
@@ -38,13 +40,14 @@
         public void Create_FirstTrueSecondFalse_ReturnsQuestion()
         {
             // Arrange
+            int amount = 2;
             TrueAnswers trueAnswers = TrueAnswers.FirstAnswer;
 
             // Act
-            IChooseQuestion actual = ChooseQuestionFactory.Create(0, 2, trueAnswers);
+            IChooseQuestion actual = ChooseQuestionFactory.Create(0, amount, trueAnswers);
 
             // Assert
-            actual.Answers.Count.Should().Be(2);
+            actual.Answers.Count.Should().Be(amount);
             actual.Answers[0].IsCorrect.Should().BeTrue();
             actual.Answers[1].IsCorrect.Should().BeFalse();
         }
@@ -53,13 +56,14 @@
         public void Create_FirstFalseSecondTrue_ReturnsQuestion()
         {
             // Arrange
+            int amount = 2;
             TrueAnswers trueAnswers = TrueAnswers.SecondAnser;
 
             // Act
-            IChooseQuestion actual = ChooseQuestionFactory.Create(0, 2, trueAnswers);
+            IChooseQuestion actual = ChooseQuestionFactory.Create(0, amount, trueAnswers);
 
             // Assert
-            actual.Answers.Count.Should().Be(2);
+            actual.Answers.Count.Should().Be(amount);
             actual.Answers[0].IsCorrect.Should().BeFalse();
             actual.Answers[1].IsCorrect.Should().BeTrue();
         }
@@ -68,13 +72,14 @@
         public void Create_FirstTrueSecondFalseThirdFalse_ReturnsQuestion()
         {
             // Arrange
+            int amount = 3;
             TrueAnswers trueAnswers = TrueAnswers.FirstAnswer;
 
             // Act
-            IChooseQuestion actual = ChooseQuestionFactory.Create(0, 3, trueAnswers);
+            IChooseQuestion actual = ChooseQuestionFactory.Create(0, amount, trueAnswers);
 
             // Assert
-            actual.Answers.Count.Should().Be(3);
+            actual.Answers.Count.Should().Be(amount);
             actual.Answers[0].IsCorrect.Should().BeTrue();
             actual.Answers[1].IsCorrect.Should().BeFalse();
             actual.Answers[2].IsCorrect.Should().BeFalse();
@@ -84,13 +89,14 @@
         public void Create_FirstTrueSecondTrueThirdFalse_ReturnsQuestion()
         {
             // Arrange
+            int amount = 3;
             TrueAnswers trueAnswers = TrueAnswers.FirstAnswer | TrueAnswers.SecondAnser;
 
             // Act
-            IChooseQuestion actual = ChooseQuestionFactory.Create(0, 3, trueAnswers);
+            IChooseQuestion actual = ChooseQuestionFactory.Create(0, amount, trueAnswers);
 
             // Assert
-            actual.Answers.Count.Should().Be(3);
+            actual.Answers.Count.Should().Be(amount);
             actual.Answers[0].IsCorrect.Should().BeTrue();
             actual.Answers[1].IsCorrect.Should().BeTrue();
             actual.Answers[2].IsCorrect.Should().BeFalse();
@@ -100,16 +106,71 @@
         public void Create_FirstTrueSecondTrueThirdTrue_ReturnsQuestion()
         {
             // Arrange
+            int amount = 3;
             TrueAnswers trueAnswers = TrueAnswers.FirstAnswer | TrueAnswers.SecondAnser | TrueAnswers.ThirdAnswer;
 
             // Act
-            IChooseQuestion actual = ChooseQuestionFactory.Create(0, 3, trueAnswers);
+            IChooseQuestion actual = ChooseQuestionFactory.Create(0, amount, trueAnswers);
 
             // Assert
-            actual.Answers.Count.Should().Be(3);
+            actual.Answers.Count.Should().Be(amount);
             actual.Answers[0].IsCorrect.Should().BeTrue();
             actual.Answers[1].IsCorrect.Should().BeTrue();
             actual.Answers[2].IsCorrect.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Create_FourAnswersFalse_ReturnsQuestion()
+        {
+            // Arrange
+            int amount = 4;
+            TrueAnswers trueAnswers = TrueAnswers.None;
+
+            // Act
+            IChooseQuestion actual = ChooseQuestionFactory.Create(0, amount, trueAnswers);
+
+            // Assert
+            actual.Answers.Count.Should().Be(amount);
+            actual.Answers[0].IsCorrect.Should().BeFalse();
+            actual.Answers[1].IsCorrect.Should().BeFalse();
+            actual.Answers[2].IsCorrect.Should().BeFalse();
+            actual.Answers[3].IsCorrect.Should().BeFalse();
+        }
+
+        [Fact]
+        public void Create_FourAnswersTrue_ReturnsQuestion()
+        {
+            // Arrange
+            int amount = 4;
+            TrueAnswers trueAnswers = TrueAnswers.FirstAnswer | TrueAnswers.SecondAnser | TrueAnswers.ThirdAnswer | TrueAnswers.FourthAnswer;
+
+            // Act
+            IChooseQuestion actual = ChooseQuestionFactory.Create(0, amount, trueAnswers);
+
+            // Assert
+            actual.Answers.Count.Should().Be(amount);
+            actual.Answers[0].IsCorrect.Should().BeTrue();
+            actual.Answers[1].IsCorrect.Should().BeTrue();
+            actual.Answers[2].IsCorrect.Should().BeTrue();
+            actual.Answers[3].IsCorrect.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Create_FirstTrueSecondFalseThirdTrueFourthFalse_ReturnsQuestion()
+        {
+            // Arrange
+            int amount = 4;
+            TrueAnswers trueAnswers = TrueAnswers.FirstAnswer | TrueAnswers.ThirdAnswer;
+
+            // Act
+            IChooseQuestion actual = ChooseQuestionFactory.Create(0, amount, trueAnswers);
+
+            // Assert
+            actual.Answers.Count.Should().Be(amount);
+            actual.Answers[0].IsCorrect.Should().BeTrue();
+            actual.Answers[1].IsCorrect.Should().BeFalse();
+            actual.Answers[2].IsCorrect.Should().BeTrue();
+            actual.Answers[3].IsCorrect.Should().BeFalse();
         }
     }
 }
