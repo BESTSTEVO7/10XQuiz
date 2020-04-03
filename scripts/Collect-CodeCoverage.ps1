1e3a403363579cc09f7912192f05cf9defe4f865
Flash0ver <38893694+Flash0ver@users.noreply.github.com>
2020-04-03 22:32:22 +0200

    add code coverage report

---
+++ scripts/Collect-CodeCoverage.ps1
@@ -0,0 +1,32 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string]$Configuration= 'Release'
+)
+
+
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$RootPath = (Get-Item -Path $PSScriptRoot).Parent
+$SolutionFilePath = Join-Path -Path $RootPath -ChildPath 'Quiz.sln'
+$NuGetConfigFilePath = Join-Path -Path $RootPath -ChildPath 'nuget.config'
+$RunSettingsFilePath = Join-Path -Path $RootPath -ChildPath 'datacollection.runsettings'
+$TestResultsDirectoryPath = Join-Path -Path $RootPath -ChildPath 'TestResults'
+
+if (Test-Path -Path $TestResultsDirectoryPath) {
+    Remove-Item -Path $TestResultsDirectoryPath -Recurse
+}
+
+dotnet tool restore
+
+dotnet restore $SolutionFilePath --configfile $NuGetConfigFilePath
+dotnet build $SolutionFilePath --configuration $Configuration --no-restore
+dotnet test $SolutionFilePath --configuration $Configuration --collect:"XPlat Code Coverage" --no-build --results-directory $TestResultsDirectoryPath --settings $RunSettingsFilePath
+
+$CoverageReports = "$($TestResultsDirectoryPath)\*\coverage.cobertura.xml"
+$TargetDirectory = Join-Path -Path $TestResultsDirectoryPath -ChildPath 'coveragereport'
+dotnet reportgenerator -reports:$CoverageReports -targetdir:$TargetDirectory -reporttypes:Html
+
+$ReportFilePath = Join-Path -Path $TargetDirectory -ChildPath 'index.htm'
+Invoke-Item -Path $ReportFilePath
